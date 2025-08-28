4b910e93eba9095c78371ed05a59cd8af0e11b14
CHECHO-c <baloncestoentablon@gmail.com>
2025-08-28 00:21:22 -0500

    Correxion de los require

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import 'dotenv/config';
+import dotenv from  'dotenv';
 import cors from 'cors';
 import ciudadano from './src/modules/ciudadano/ciudadano.routes.js';
 import usuario from './src/modules/usuario/usuario.routes.js';
@@ -14,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-require('dotenv').config();
+dotenv.config();
 
 // se globaliza la carpeta donde se guardan las imagenes de los qr
 app.use('/qrcodes', express.static(__dirname + '/qrcode'));
